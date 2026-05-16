769803dd5717a047313cde86f47e24eb0650f590
CTChris <aaa9967898@gmail.com>
2026-05-17 01:22:47 +0800

    refactor(python): make calendar name constants immutable

--- python/src/mayan_calc/constants.py
+++ python/src/mayan_calc/constants.py
@@ -22,16 +22,16 @@ MEEUS_MONTH_FACTOR = 30.6001
 MEEUS_EPOCH_A = 4716
 MEEUS_EPOCH_B = 1524
 
-TZOLKIN_DAY_SIGNS = [
+TZOLKIN_DAY_SIGNS = (
     "Imix", "Ik'", "Ak'bal", "K'an", "Chikchan",
     "Kimi", "Manik'", "Lamat", "Muluk", "Ok",
     "Chuwen", "Eb", "Ben", "Hix", "Men",
     "Kib", "Kaban", "Etz'nab", "Kawak", "Ajaw",
-]
+)
 
-HAAB_MONTHS = [
+HAAB_MONTHS = (
     "Pop", "Wo", "Sip", "Sotz'", "Sek",
     "Xul", "Yaxk'in", "Mol", "Ch'en", "Yax",
     "Sak", "Keh", "Mak", "K'ank'in", "Muwan",
     "Pax", "K'ayab", "Kumk'u", "Wayeb",
-]
+)
--- python/tests/test_constants.py
+++ python/tests/test_constants.py
@@ -11,8 +11,16 @@ def test_tzolkin_sign_count():
     assert len(TZOLKIN_DAY_SIGNS) == 20
 
 
+def test_tzolkin_signs_are_immutable_tuple():
+    assert isinstance(TZOLKIN_DAY_SIGNS, tuple)
+
+
 def test_haab_month_count():
     assert len(HAAB_MONTHS) == 19  # 18 months + Wayeb
+
+
+def test_haab_months_are_immutable_tuple():
+    assert isinstance(HAAB_MONTHS, tuple)
 
 
 def test_gmt_correlation_value():
@@ -44,20 +52,20 @@ def test_haab_includes_kumku():
 
 
 def test_tzolkin_full_order():
-    expected = [
+    expected = (
         "Imix", "Ik'", "Ak'bal", "K'an", "Chikchan",
         "Kimi", "Manik'", "Lamat", "Muluk", "Ok",
         "Chuwen", "Eb", "Ben", "Hix", "Men",
         "Kib", "Kaban", "Etz'nab", "Kawak", "Ajaw",
-    ]
+    )
     assert TZOLKIN_DAY_SIGNS == expected
 
 
 def test_haab_full_order():
-    expected = [
+    expected = (
         "Pop", "Wo", "Sip", "Sotz'", "Sek",
         "Xul", "Yaxk'in", "Mol", "Ch'en", "Yax",
         "Sak", "Keh", "Mak", "K'ank'in", "Muwan",
         "Pax", "K'ayab", "Kumk'u", "Wayeb",
-    ]
+    )
     assert HAAB_MONTHS == expected
